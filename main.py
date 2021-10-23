e935d850284a1ddf5d964cd120d00590469e0e90
Ben Clifford <benc@hawaga.org.uk>
2021-10-23 10:06:31 +0100

    move colour related helpers to own file

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import time
 import flask
 
 import swirl.randomwalk as randomwalk
+from swirl.colour import different_hue, gamma, hsv_to_neo_rgb, scale
 
 from functools import partial
 from math import tau
@@ -29,39 +30,6 @@ bottoms = [50, 49, 46, 37, 22, 0]
 new_mode = None
 
 disco_thread = None
-
-def scale(f):
-    """ scale from 0..1 : float -> 0..255 : int"""
-    try:
-      return int(f * 255)
-    except:
-      print("Scale got an exception on input {}".format(f))
-      reraise
-
-
-def gamma(v, gamma_factor=1.9):
-    """ gamma correct on range 0..1 : float
-    Based on https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
-
-    gamma_factor default of 1.9 comes from my subjective attempts to get the colourwheel to look even.
-
-    gamma_factor 2.8 is the value that adafruit people like
-
-    """
-    if v < 0 or v > 1:
-        raise ValueError("Attempted gamma correction on out of range value {}".format(v))
-
-    return (v ** gamma_factor)
-
-
-def hsv_to_neo_rgb(h, s=1, v=1):
-    """Convert specified HSV values to neopixel compatible RGB. S and V default
-    to full brightness, fully saturated, as that is a common use mode
-    for designs.
-    """
-    (red, green, blue) = colorsys.hsv_to_rgb(h, s, v)
-    return ( scale(gamma(red)), scale(gamma(green)), scale(gamma(blue)) )
-
 
 def pixel_to_layer(pixel):
     """Given a pixel, return the loop number and fraction around the
@@ -76,12 +44,6 @@ def pixel_to_layer(pixel):
     else:
       raise RuntimeError("could not find range for pixel {}".format(pixel))
     return (b, frac)
-
-
-def different_hue(hue):
-    """Returns a hue that is noticeably different than the
-    input hue."""
-    return (hue + 0.15 + random.random()*0.7) % 1.0
 
 
 def mode1():
---
+++ swirl/colour.py
@@ -0,0 +1,42 @@
+import colorsys
+import random
+
+def scale(f):
+    """ scale from 0..1 : float -> 0..255 : int"""
+    try:
+      return int(f * 255)
+    except:
+      print("Scale got an exception on input {}".format(f))
+      reraise
+
+def gamma(v, gamma_factor=1.9):
+    """ gamma correct on range 0..1 : float
+    Based on https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+
+    gamma_factor default of 1.9 comes from my subjective attempts to get the colourwheel to look even.
+
+    gamma_factor 2.8 is the value that adafruit people like
+
+    """
+    if v < 0 or v > 1:
+        raise ValueError("Attempted gamma correction on out of range value {}".format(v))
+
+    return (v ** gamma_factor)
+
+def hsv_to_neo_rgb(h, s=1, v=1):
+    """Convert specified HSV values to neopixel compatible RGB. S and V default
+    to full brightness, fully saturated, as that is a common use mode
+    for designs.
+    """
+    (red, green, blue) = colorsys.hsv_to_rgb(h, s, v)
+    return ( scale(gamma(red)), scale(gamma(green)), scale(gamma(blue)) )
+
+
+
+def different_hue(hue):
+    """Returns a hue that is noticeably different than the
+    input hue."""
+    return (hue + 0.15 + random.random()*0.7) % 1.0
+
+
+
