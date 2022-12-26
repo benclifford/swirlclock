4a356a363e1b24ff2568daeb9be37c6d5cf123b7
Ben Clifford <benc@hawaga.org.uk>
2022-12-26 16:01:48 +0000

    Oscillates a lot, coloured cyan for intensity and red for rate of change

--- julia/first.jl
+++ julia/first.jl
@@ -23,18 +23,16 @@ end
 
 leds = fill(RGBf(0,0,0), 50)
 
+magnitude = fill(Float64(0), 50)
 rate_of_change = fill(Float64(0), 50)
 
 for i in 1:50
-  l = leds[i]
-  # l = @set l.blue = rand()
-  if rand() < 0.1
-    l = @set l.blue = 1
+  # if rand() < 0.03
+  if i == 25
+    magnitude[i] = 10
   else
-    l = @set l.blue = 0
+    magnitude[i] = 0
   end
-
-  leds[i] = l
 end
 
 while true
@@ -43,58 +41,72 @@ while true
   end
   println("")
 
+  new_magnitude = fill(Float64(0), 50)
   
-  newleds = fill(RGBf(0,0,0), 50)
-
   maximum = 0
   minimum = 1
+  tot = 0
 
   for i in 1:50
     # This syntax for immutable updates is more complicated than
     # I like.
 
-    l = leds[i]
+    l = magnitude
+
+    decay = 100
 
     if i > 1
       ix = i -1
+      d_l = (magnitude[i] - magnitude[ix]) / decay
     else
       ix = 50
+      # d_l = (magnitude[i] - magnitude[ix]) / decay
+      d_l = 0
     end
 
-    d_l = (leds[i].blue - leds[ix].blue) / 30
 
     if i < 50
       ix  = i + 1
+      d_r = (magnitude[i] - magnitude[ix]) / decay
     else
       ix = 1
+      # d_r = (magnitude[i] - magnitude[ix]) / decay
+      d_r = 0
     end
 
-    d_r = (leds[i].blue - leds[ix].blue) / 30
 
-    rate_of_change[i] = rate_of_change[i] - d_l - d_r
+    rate_of_change[i] = (rate_of_change[i] * 1.0) - d_l - d_r
 
-    l = @set l.blue = l.blue + rate_of_change[i]
+    nv = magnitude[i] + rate_of_change[i]
+    new_magnitude[i] = nv
 
-    l = @set l.blue = max(0,l.blue)
-    l = @set l.blue = min(1,l.blue)
+    intensity = nv
+    intensity = max(0, intensity)
+    intensity = min(1, intensity)
 
-    l = @set l.red = l.blue
-    l = @set l.green = l.blue
+    roc = rate_of_change[i] * decay / 10
+    # println(stderr, roc)
+    roc = max(0, roc)
+    roc = min(1, roc)
 
-    newleds[i] = l
-    if l.blue > maximum
-      maximum = l.blue
+    leds[i] = RGBf(roc, intensity, intensity)
+
+    if nv > maximum
+      maximum = nv
     end
-    if l.blue < minimum
-      minimum = l.blue
+    if nv < minimum
+      minimum = nv
     end
+    tot = tot + nv
   end
 
-  global leds = newleds
+  global magnitude = new_magnitude
 
   print(stderr, minimum)
   print(stderr, " .. ")
-  println(stderr, maximum)
+  print(stderr, maximum)
+  print(stderr, " => ")
+  println(stderr, tot)
 
-  sleep(0.1)
+  sleep(0.05)
 end
